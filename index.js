ad1d91668d5eebd100778ac2ea45e7a0b139b6d2
Ian Cenry <iancenry@gmail.com>
2023-03-21 21:33:16 +0300

    config gpt req/res

--- index.js
+++ index.js
@@ -18,11 +18,21 @@ const bot = new TelegramBot(token, {polling: true});
 
 
 // Listen for any kind of message. There are different kinds of messages.
-bot.on('message', (msg) => {
+bot.on('message', async (msg) => {
   const chatId = msg.chat.id;
   const msgContent = msg.text
 
-  //TODO add middleware
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt : msgContent,
+    temperature: 0,
+    max_tokens: 100,
+    top_p: 1,
+    frequency_penalty: 0.0,
+    presence_penalty: 0.0,
+  })
+  
+  bot.sendMessage(chatId, response.data.choices[0].text);
 
   bot.sendMessage(chatId, 'Received your message');
 });
