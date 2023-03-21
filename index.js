a92063847c39d1652e2b5cb1066670c017d8f5f6
Ian Cenry <iancenry@gmail.com>
2023-03-21 21:41:33 +0300

    add comments

--- index.js
+++ index.js
@@ -3,17 +3,24 @@ const TelegramBot = require('node-telegram-bot-api');
 const {Configuration, OpenAIApi} = require('openai')
 const dotenv = require('dotenv')
 
+//init express
 const app = express();
+
+//setup environment variables
 dotenv.config();
 
+//setup telegram bot access token
 const token = process.env.TELEGRAM_BOT_TOKEN;
 
+//config openai API
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 })
 
+//new openai instance
 const openai = new OpenAIApi(configuration)
 
+//create a new bot with polling
 const bot = new TelegramBot(token, {polling: true});
 
 bot.on("polling_error", (msg) => console.log(msg));
@@ -23,6 +30,7 @@ bot.on('message', async (msg) => {
   const chatId = msg.chat.id;
   const msgContent = msg.text
 
+  // send message received to openai api
   const response = await openai.createCompletion({
     model: "text-davinci-003",
     prompt : msgContent,
@@ -32,14 +40,11 @@ bot.on('message', async (msg) => {
     frequency_penalty: 0.0,
     presence_penalty: 0.0,
   })
-  
+
+  //send response from openai api to telegram bot user
   bot.sendMessage(chatId, response.data.choices[0].text);
 });
 
-
-
-
 const PORT = process.env.PORT || 5000;
-
-
+//start express server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
